d08ef9534d5395b7687a43e5bc35462b49eaa883
jini4847-ship-it <jini4847@gmail.com>
2026-07-01 21:31:02 +0900

    animals.js 업데이트

--- animals.js
+++ animals.js
@@ -3,24 +3,16 @@ const animals = [
     id: "lion",
     name: "사자",
     emoji: "🦁",
-    image: "https://images.unsplash.com/photo-1546182990-dffeafbe841d?w=800"
+    images: [
+      "https://images.unsplash.com/photo-1546182990-dffeafbe841d?w=1200",
+      "https://images.unsplash.com/photo-1511216113906-8f57bb83e?w=1200",
+      "https://images.unsplash.com/photo-1534188753412-3e26d0d618d6?w=1200"
+    ]
   },
   {
     id: "tiger",
     name: "호랑이",
     emoji: "🐯",
-    image: "https://images.unsplash.com/photo-1549366021-9f761d450615?w=800"
-  },
-  {
-    id: "elephant",
-    name: "코끼리",
-    emoji: "🐘",
-    image: "https://images.unsplash.com/photo-1557050543-4d5f4e07ef46?w=800"
-  },
-  {
-    id: "panda",
-    name: "판다",
-    emoji: "🐼",
-    image: "https://images.unsplash.com/photo-1564349683136-77e08dba1ef7?w=800"
-  }
-];
+    images: [
+      "https://images.unsplash.com/photo-1549366021-9f761d450615?w=1200",
+      "https://images.unsplash
