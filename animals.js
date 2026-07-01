163150ef589691ad42bbeee35687dd5b1167fbb4
jini4847-ship-it <jini4847@gmail.com>
2026-07-01 10:01:35 +0900

    animals.js 만들기

---
+++ animals.js
@@ -0,0 +1,24 @@
+const animals = [
+  {
+    id: "lion",
+    name: "사자",
+    english: "Lion",
+    emoji: "🦁",
+    images: [
+      "https://images.unsplash.com/photo-1546182990-dffeafbe841d?w=1200",
+      "https://images.unsplash.com/photo-1511216113906-8f57bb83e776?w=1200",
+      "https://images.unsplash.com/photo-1534188753412-3e26d0d618d6?w=1200"
+    ]
+  },
+  {
+    id: "tiger",
+    name: "호랑이",
+    english: "Tiger",
+    emoji: "🐯",
+    images: [
+      "https://images.unsplash.com/photo-1549366021-9f761d450615?w=1200",
+      "https://images.unsplash.com/photo-1561731216-c3a4d99437d5?w=1200",
+      "https://images.unsplash.com/photo-1549480017-d76466a4b7d3?w=1200"
+    ]
+  }
+];
