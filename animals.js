bea462cb3f14087a2a779d0672d044353837f9ea
jini4847-ship-it <jini4847@gmail.com>
2026-07-02 20:13:24 +0900

    animals.js 업데이트

--- animals.js
+++ animals.js
@@ -3,80 +3,60 @@ const animals = [
     id: "lion",
     name: "사자",
     emoji: "🦁",
-    images: [
-      "https://images.unsplash.com/photo-1546182990-dffeafbe841d?w=800"
-    ]
+    search: "lion"
   },
   {
     id: "tiger",
     name: "호랑이",
     emoji: "🐯",
-    images: [
-      "https://images.unsplash.com/photo-1549366021-9f761d450615?w=800"
-    ]
+    search: "tiger"
   },
   {
     id: "elephant",
     name: "코끼리",
     emoji: "🐘",
-    images: [
-      "https://images.unsplash.com/photo-1557050543-4d5f4e07ef46?w=800"
-    ]
+    search: "elephant"
   },
   {
     id: "giraffe",
     name: "기린",
     emoji: "🦒",
-    images: [
-      "https://images.unsplash.com/photo-1547721064-da6cfb341d50?w=800"
-    ]
+    search: "giraffe"
   },
   {
     id: "panda",
     name: "판다",
     emoji: "🐼",
-    images: [
-      "https://images.unsplash.com/photo-1564349683136-77e08dba1ef7?w=800"
-    ]
+    search: "panda"
   },
   {
     id: "dog",
     name: "강아지",
     emoji: "🐶",
-    images: [
-      "https://images.unsplash.com/photo-1517849845537-4d257902454a?w=800"
-    ]
+    search: "dog"
   },
   {
     id: "cat",
     name: "고양이",
     emoji: "🐱",
-    images: [
-      "https://images.unsplash.com/photo-1519052537078-e6302a4968d4?w=800"
-    ]
+    search: "cat"
   },
   {
     id: "rabbit",
     name: "토끼",
     emoji: "🐰",
-    images: [
-      "https://images.unsplash.com/photo-1585110396000-c9ffd4e4b308?w=800"
-    ]
+    search: "rabbit"
   },
   {
     id: "bear",
     name: "곰",
     emoji: "🐻",
-    images: [
-      "https://images.unsplash.com/photo-1589656966895-2f33e7653819?w=800"
-    ]
+    search: "bear"
   },
   {
     id: "zebra",
     name: "얼룩말",
     emoji: "🦓",
-    images: [
-      "https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?w=800"
-    ]
+    search: "zebra"
   }
 ];
