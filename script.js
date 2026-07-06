c6ef271e9932345309da6ef12fca3b4e933deb02
jini4847-ship-it <jini4847@gmail.com>
2026-07-06 18:46:05 +0900

    script.js 업데이트

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
 const animalList = document.getElementById("animalList");
 
-animals.forEach((animal) => {
+animals.forEach(async (animal) => {
+
   const card = document.createElement("div");
   card.className = "card";
 
   card.innerHTML = `
-    <div class="emoji" style="font-size:70px;text-align:center;margin-top:20px;">
-      ${animal.emoji}
-    </div>
-    <div class="info">
-      <div class="name">${animal.name}</div>
-    </div>
+      <div class="emoji">${animal.emoji}</div>
+      <img class="thumb" src="" alt="${animal.name}">
+      <div class="info">
+          <div class="name">${animal.name}</div>
+      </div>
   `;
 
   card.onclick = () => {
-    location.href = `gallery.html?id=${animal.id}`;
+      location.href=`gallery.html?id=${animal.id}`;
   };
 
   animalList.appendChild(card);
+
+  try{
+
+      const search =
+      animal.searches[Math.floor(Math.random()*animal.searches.length)];
+
+      const res = await fetch(
+      `https://api.pexels.com/v1/search?query=${encodeURIComponent(search)}&per_page=1`,
+      {
+          headers:{
+              Authorization:PEXELS_API_KEY
+          }
+      });
+
+      const data = await res.json();
+
+      if(data.photos.length>0){
+
+          card.querySelector(".thumb").src =
+          data.photos[0].src.medium;
+
+      }
+
+  }catch(e){
+
+      console.log(e);
+
+  }
+
 });
