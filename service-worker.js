e179707bf8cfb911c3eaf6db07587190b2104e9a
jini4847-ship-it <jini4847@gmail.com>
2026-07-17 08:27:20 +0900

    service-worker.js 만들기

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "animal-friend-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./gallery.html",
+  "./style.css",
+  "./animals.js",
+  "./script.js",
+  "./gallery.js",
+  "./config.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
